054a0d84e1ecd7eae1b3ea688e96ca133304b950
Ludilson Souza <79519422+LudilsonSouza@users.noreply.github.com>
2022-05-24 13:32:10 -0300

    #ABOUT
    Configuração do about.

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
+// Configurarando o click abrir e fechar menu
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
-  element.addEventListener('click', function() {
+  element.addEventListener('click', function () {
     nav.classList.toggle('show')
-  }) 
+  })
 }
 
+// Configurando o click do item menu
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
